e7cb8f1b76ef1d499fc4106d6028bbf0630ee0cd
taehee-kim-dev <taehee.kim.dev@gmail.com>
2024-02-06 23:20:14 +0900

    practice: 10번 문제까지

---
+++ src/EX_IncrementAndDecrementOperator.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>): Unit {
+    var a = 10
+    var b = 5
+
+    println(a++ + b) // 15
+    println(a) // 11
+    println(--b) // 4
+}
---
+++ src/EX_LiteralDataType.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>): Unit {
+    val variable = 10 + 12 - 5
+    println(variable)
+}
